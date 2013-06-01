c2ed086e7756a5a22f72dba84513f2ce6dad7285
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-01 09:58:38 +0530

    Created a schema for contributors

---
+++ constants/contributors.js
@@ -0,0 +1,27 @@
+exports.contributors = {
+    
+    admin : {
+        
+        name : "Akshat Jiwan Sharma",
+        secret : 'dont#fail@',
+        about : ''
+        
+    },
+    
+    users : [
+        
+        {
+            name : "Not an admin",
+            secret : 'get@creative#',
+            about : ''
+        }
+    ],
+    
+    guest : [
+        {
+            name : 'A guest',
+            secret : 'by!invitation&',
+            website : ''
+        }
+    ]
+};
