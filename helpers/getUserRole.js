36f4071bcadd274488a3329bfef2dc653a6a2f29
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-01 22:49:36 +0530

    simplified the getuserrole method immensly

--- helpers/getUserRole.js
+++ helpers/getUserRole.js
@@ -1,18 +1,27 @@
 var constants = require('../constants');
 var contributors = constants.contributors;
 
-exports.getRoleFromSecret = function(secret){
+exports.getRoleFromSecret = function(secret){ 
+        
+        var user = getUserFromSecret(secret);
+        var guest = getGuestFromSecret(secret);
+        var admin = getAdminFromSecret(secret);
     
-    if(contributors.admin.secret===secret){
-        
-        return contributors.admin; 
-    }
+    return admin ? admin : user? user:guest?guest:false;
     
-    else{
-        
-        var user = contributors.users.filter(function(user){return user.secret===secret});
-        var guest = contributors.guests.filter(function(guest){return guest.secret===secret});
-        
-    }
-    return false;
 };
+
+function getUserFromSecret(secret){
+    
+    return contributors.users.filter(function(user){return user.secret===secret;});
+}
+
+function getAdminFromSecret(secret){
+    
+   return contributors.admins.filter(function(admin){return admin.secret===secret;});
+}
+
+function getGuestFromSecret(secret){
+    
+    return contributors.guests.filter(function(guest){return guest.secret===secret;});
+}
