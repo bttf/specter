1ee66c600d093102860c10c1399ab01dec6e76f1
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-01 10:26:07 +0530

    extracted the constants variable

--- routes/create.js
+++ routes/create.js
@@ -1,10 +1,10 @@
 var constants = require('../constants');
-
+var contributors = constants.contributors;
 
 exports.newPost = function(req,res){
     return res.render(constants.views.createPost);
 };
 
 exports.addPost = function(req,res){
-    console.log(constants.contributors);
+    
 };
