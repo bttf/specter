19f9709fb721b42a3308fde3cd665e6de12ba8d3
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-01 10:19:51 +0530

    changed the very of add post to post

--- routeConfiguration.js
+++ routeConfiguration.js
@@ -3,7 +3,9 @@ var routes = require('./routes');
 exports.defineRoutes= function(app){
     
 app.get('/create',routes.create.newPost);
-app.get('/addpost',routes.create.addPost);    
+    
+    
+app.post('/addpost',routes.create.addPost);    
     
     
 };
