d040ec778246347eda405398dab9a14647ee6be9
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-04 23:44:38 +0530

    broke the index route into 2
    for paging

--- routeConfiguration.js
+++ routeConfiguration.js
@@ -4,7 +4,7 @@ exports.defineRoutes= function(app){
     
 app.get('/create',routes.create.newPost);
 app.get('/:page',routes.recentPosts.getRecentPosts);    
-    
+app.get('/',routes.recentPosts.getRecentPosts);    
 app.post('/addpost',routes.create.addPost);    
     
     
