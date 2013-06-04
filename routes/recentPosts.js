5d5cde58484c0156fea4128597d7fb7775b04062
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-04 20:08:10 +0530

    fixed compile errors

--- routes/recentPosts.js
+++ routes/recentPosts.js
@@ -1,7 +1,12 @@
 var constants = require('../constants');
 var request = require('request');
+var helpers = require('../helpers');
 
 exports.getRecentPosts = function(req,res){
+    
+    var url = constants.queries.recentPosts();
+    
+    var headers = helpers.setHeaders(url,getRecentPostsQueryData());
     
     return res.render(constants.views.home);
 }
