d72761519876a09a9da0ba429539a489ceee226e
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-05 18:13:14 +0530

    Got the post detail right here

--- routes/postDetail.js
+++ routes/postDetail.js
@@ -1,8 +1,12 @@
 var constants = require('../constants');
+var request = require('request');
 
 exports.postDetail = function(req,res){
   
     var id = req.params.id;
-    
+    var url = constants.queries.postType()+id;
+    request(url,function(error,response,body){        
+        console.log(body);
+    });
     
 };
