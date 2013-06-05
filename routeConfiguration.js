093204c25f243ece4f2e63444c6e076e76232def
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-05 17:59:57 +0530

    Modified the route
    This is a bit selfish. I want specter routes to be exactly the same as
    the routes of my personal blog :)

--- routeConfiguration.js
+++ routeConfiguration.js
@@ -3,8 +3,8 @@ var routes = require('./routes');
 exports.defineRoutes= function(app){
     
 app.get('/create',routes.create.newPost);
-app.get('/post/:id',routes.postDetail.postDetail);    
-app.get('/:page',routes.recentPosts.getRecentPosts);    
+app.get('/:id',routes.postDetail.postDetail);    
+app.get('/page/:page',routes.recentPosts.getRecentPosts);    
 app.get('/',routes.recentPosts.getRecentPosts);    
 app.post('/addpost',routes.create.addPost);
     
