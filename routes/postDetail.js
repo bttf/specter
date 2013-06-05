1d4c3a105ceaf59c8eb653b2bbde59a791ef34f6
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-05 23:19:55 +0530

    Post detail render complete

--- routes/postDetail.js
+++ routes/postDetail.js
@@ -5,9 +5,10 @@ exports.postDetail = function(req,res){
   
     var id = req.params.id;
     var url = constants.queries.postType()+id;
-    request(url,function(error,response,body){        
-        body._source.postedOn = new Date(body._source.postedOn).toDateString();
-        return res.render(constants.views.postDetail,body);
+    request(url,function(error,response,body){
+        var parsed = JSON.parse(body);      
+        parsed._source.postedOn = new Date(parsed._source.postedOn).toDateString();
+        return res.render(constants.views.postDetail,parsed);
     });
     
 };
