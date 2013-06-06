dcffbc630bb53f94d994ae940185f098106ed79e
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-06 18:50:30 +0530

    changed VERB to get

--- routeConfiguration.js
+++ routeConfiguration.js
@@ -3,11 +3,12 @@ var routes = require('./routes');
 exports.defineRoutes= function(app){
     
 app.get('/create',routes.create.newPost);
-app.get('/:id',routes.postDetail.postDetail);    
+app.get('/:id',routes.postDetail.postDetail);
+app.get('/:id/edit',routes.edit.editPost);    
 app.get('/page/:page',routes.recentPosts.getRecentPosts);    
 app.get('/',routes.recentPosts.getRecentPosts);
 app.post('/searchByTitle',routes.search.searchByTitle);    
 app.post('/addpost',routes.create.addPost);
-app.post('/:id/edit',routes.edit.editPost);    
+    
     
 };
