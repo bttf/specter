975bc38a5f377dc0823eb50290aee93c7252fadb
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-06 12:22:40 +0530

    search called if length less than equal to 3

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -2,23 +2,26 @@ var searchViewModel = function(){
     var self = this;
     self.searchText = ko.observable();
     self.searchResults = ko.observableArray();
-    self.hasAppropriateLength = ko.computed(function(){
-        
-        return self.searchText().length>3 ? true:false;
-    });
+    
     self.search = function(){
         var postData = {};
         var query = self.searchText();
         postData.query = query;
         console.log(self.hasAppropriateLength());
-        if(self.hasAppropriateLength()){
+        
             
             $.post('/searchByTitle',postData,function(data){
                 
                 self.searchResults(data);
             });
-        }
+        
     };
+    
+    self.hasAppropriateLength = ko.computed(function(){
+        
+        return self.searchText()? self.searchText().length>=3 ? self.search():false:false;
+    });
+    
     
     
      
