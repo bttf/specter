e6918e08363c57b4afad5dd9b23ec76f28b90d5b
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-06 12:07:51 +0530

    fixed js hint errors

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -2,26 +2,26 @@ var searchViewModel = function(){
     var self = this;
     self.searchText = ko.observable();
     self.searchResults = ko.observableArray();
-    self.hasAppropriateLength = ko.computed(){
+    self.hasAppropriateLength = ko.computed(function(){
         
         return self.searchText().length>3 ? true:false;
-    }
+    });
     self.search = function(){
         var postData = {};
         var query = self.searchText();
         postData.query = query;
-        console.log(self.hasAppropriateLength())
+        console.log(self.hasAppropriateLength());
         if(self.hasAppropriateLength()){
             
             $.post('/searchByTitle',postData,function(data){
                 
                 self.searchResults(data);
-            })
+            });
         }
     };
     
     
      
-}
+};
 
 ko.applyBindings(new searchViewModel());
