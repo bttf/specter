caff9bc66a46a85805adff6b0a16ae938c929f85
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-06 19:53:43 +0530

    Removed figure caption on the server side

--- routes/edit.js
+++ routes/edit.js
@@ -1,24 +1,33 @@
 var tomarkdown = require('to-markdown').toMarkdown;
 var constants = require('../constants');
 var request = require('request');
+var cheerio = require('cheerio');
+var helpers = require('../helpers');
 
 exports.editPost = function(req,res){
     
     var id = req.params.id;
     var url = constants.queries.postType() + id;
-    console.log(url);
-    request(url,function(error,response,body){
+     request(url,function(error,response,body){
         if(!body) return res.send(404);
         var parsed = JSON.parse(body);
-        var markdownText = getMarkdownFromHtml(parsed._source.postHtml);
+        var html = removeFigureCaption( parsed._source.postHtml);
+        var stripped = helpers.stripHtml(html);
+        var markdownText = getMarkdownFromHtml(stripped);
         var item = {};
         item.post = markdownText;
         item.title = parsed._source.title;
         return res.render(constants.views.createPost,item);
-    })
+    });
 };
 
 function getMarkdownFromHtml(html){
     
     return tomarkdown(html);
+}
+
+function removeFigureCaption(html){
+    var $ = cheerio.load(html);
+     $("figcaption").remove();
+    return $.html();
 }
