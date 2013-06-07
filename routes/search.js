d4b605c0a908d0fc6b5447f2ca7112b18949ab0d
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-07 19:28:09 +0530

    added dependencies

--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,11 @@
+var helpers = require('../helpers');
+var request = require('../request');
+
 exports.searchByTitle = function(req,res){
     
-    var searchQuery = req.body;
+    var searchQuery = req.body.query;
     console.log(searchQuery);
-    var response = [
-        {
-        "_id": "another-post",
-        "title" : "Another post",
-        "wordCount" : "40"
-    },
-        {
-        "_id": "test",
-        "title" : "Test",
-        "wordCount" : "700"}
-        ];
+    
     
     return res.send(response);
 };
