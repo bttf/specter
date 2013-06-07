e04e53ff5f8674838fff99f5c598e4b9978b29dd
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-07 20:57:01 +0530

    fleshed out the search route

--- routes/search.js
+++ routes/search.js
@@ -5,10 +5,12 @@ var constants = require('../constants');
 exports.searchByTitle = function(req,res){
     
     var searchQuery = req.body.query;
-   
+    var url = helpers.databaseUrl.search();
+    var searchData = buildSearchQuery(searchQuery);
+    request(helpers.setHeaders(url,searchData),function(error,response,body){
+        console.log(body);
+    })  
     
-    
-    return res.send(response);
 };
 
 function buildSearchQuery(searchTerm){
