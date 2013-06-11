36a57c5dac746764c72b5d139fcd4db2e63ce657
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-11 13:37:48 +0530

    removed slop term in match_phrase
    complete phrase or go home

--- routes/search.js
+++ routes/search.js
@@ -36,8 +36,8 @@ var query = {
                             
                             "postHtml":{
                             
-                            "query" : searchTerm,
-                            "slop" : 1    
+                            "query" : searchTerm
+                                
                          }
                     
                         }        
