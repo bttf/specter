1bf81a96207626c8d2a8ebe551bd91761365c1b0
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-12 10:53:11 +0530

    Added views to the constants

--- constants/views.js
+++ constants/views.js
@@ -2,6 +2,7 @@ exports.views = {
     
     createPost : 'Abstract/index.html',
     home : 'index.html',
-    postDetail : 'postDetail.html'
+    postDetail : 'postDetail.html',
+    deletePost : 'deletePost.html'
     
 };
