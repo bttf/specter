7ca34d04632cb152ea8632ad57ff58030fd04de7
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-14 16:43:51 +0530

    Updated with more readable condition

--- routes/recentPosts.js
+++ routes/recentPosts.js
@@ -9,7 +9,8 @@ exports.getRecentPosts = function(req,res){
     var headers = helpers.setHeaders(url,getRecentPostsQueryData());
     
     request(headers,function(error,response,body){
-        if(!(body.hasOwnProperty("hits")&&body.hits.length>0)){return res.redirect('/create');}
+	var hasPosts = body.hasOwnProperty("hits")&&body.hits.hits.length>0;
+        if(!hasPosts){return res.redirect('/create');}
         var resultCount = constants.queries.paginationSize - 1;
         var results = body.hits;
         var dataToRender = buildResponse(results.hits.slice(0,resultCount),pageNo,results.total);
