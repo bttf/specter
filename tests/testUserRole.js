02341934dc90e6a20d113b9b289d0b3e751866d2
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-19 19:33:41 +0530

    Added a test for just admin

--- tests/testUserRole.js
+++ tests/testUserRole.js
@@ -4,10 +4,19 @@ var mock = require('./mockContributors.js');
 
 exports.testAdminRole = function(test){
 	
-	
-	var contributor = helpers.getContributor.getRoleFromSecret('dont#fail@',mock.contributors)
-	
+	var contributor = helpers.getContributor.getRoleFromSecret('dont#fail@',mock.contributors);
+	var admin = {
+		admins:[
+			{
+				"name":"Akshat Jiwan Sharma",
+				'secret' : 'hey'
+			}
+		]
+	};
+	var justAdmin = helpers.getContributor.getRoleFromSecret('hey',admin)	
 	test.equal(contributor.role,'admin');
 	test.equal(contributor.details.name,'Akshat Jiwan Sharma');
+	test.equal(justAdmin.role,admin);
+	test.equal(justAdmin.details.name, 'Akshat Jiwan Sharma');
 	test.done();
 };
