914e4b210739983c91265b451b8560b44883eaca
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-19 18:58:54 +0530

    first test passing
    wo hoo

--- tests/testUserRole.js
+++ tests/testUserRole.js
@@ -1,11 +1,13 @@
 var helpers = require('../helpers');
-var mock = require('../mockContributors.js');
+var mock = require('./mockContributors.js');
 
 
 exports.testAdminRole = function(test){
 	
-	var contributor = helpers.getContributor.getRoleFromSecret(mock.contributors,'dont#fail@')
+	
+	var contributor = helpers.getContributor.getRoleFromSecret('dont#fail@',mock.contributors)
 	
 	test.equal(contributor.role,'admin');
 	test.equal(contributor.details.name,'Akshat Jiwan Sharma');
+	test.done();
 };
