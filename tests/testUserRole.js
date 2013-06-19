b413a5961fe6a2064440e61f099f8f2f1866a963
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-19 18:51:06 +0530

    fleshed out test

--- tests/testUserRole.js
+++ tests/testUserRole.js
@@ -1,2 +1,11 @@
 var helpers = require('../helpers');
 var mock = require('../mockContributors.js');
+
+
+exports.testAdminRole = function(test){
+	
+	var contributor = helpers.getContributor.getRoleFromSecret(mock.contributors,'dont#fail@')
+	
+	test.equal(contributor.role,'admin');
+	test.equal(contributor.details.name,'Akshat Jiwan Sharma');
+};
