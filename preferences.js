4a40b888de782859c1fe8311fe28ebceeb744d4c
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-28 12:30:28 +0530

    prefrences back to default

--- preferences.js
+++ preferences.js
@@ -1,7 +1,7 @@
 exports.preferences = {
 	// reference :	https://github.com/brislink/specter/wiki/preferences-documentation 
 	"paginationSize" :11, 
-	 "databaseUrl"   :"http://gotbsgju.api.qbox.io/",
+	 "databaseUrl"   :"http://localhost:9200",
 	"searchResults" :5,
 	"pageFields":["postedBy","postedOn","title","wordCount"],
 	"summaryLength" : 0
