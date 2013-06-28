91e6785de766a50ab28b3aa4c9b34fb25d1650a4
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-28 10:36:45 +0530

    works like a charm

--- routes/recentPosts.js
+++ routes/recentPosts.js
@@ -6,7 +6,7 @@ var preferences = require('../preferences').preferences;
 exports.getRecentPosts = function(req,res){
     
     var url = constants.queries.search();
-    var pageNo = parseInt(req.params.page);
+	var pageNo = parseInt(req.params.page);
     var headers = helpers.setHeaders(url,getRecentPostsQueryData(pageNo,constants.queries.paginationSize));
     
     request(headers,function(error,response,body){
@@ -22,11 +22,11 @@ exports.getRecentPosts = function(req,res){
     
 };
 
-function getRecentPostsQueryData(pageNo,paginationSize){
-    
+function getRecentPostsQueryData(pageNo,paginationSize){    
+	
     var queryData = {
       "sort" :{ "postedOn" : {"order" : "desc"}},
-	   "fields" : preferences.fields	
+	   "fields" : preferences.pageFields	
     };
 	
     return helpers.pagination.buildPaginationQuery(pageNo,paginationSize,queryData);
