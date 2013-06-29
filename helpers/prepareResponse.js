d989d5d71efa26fdcf213b6ea7f1f816950b61fa
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-29 11:58:06 +0530

    simplified the prepare response

--- helpers/prepareResponse.js
+++ helpers/prepareResponse.js
@@ -5,18 +5,18 @@ exports.prepareResponse = function (data){
 	//check preferences here.
     data.forEach(function(item,index,arr){        
       if(hasField("postedOn")){
-		  return item.fields.postedOn = new Date(item.fields.postedOn).toDateString();
+		   item.fields.postedOn = new Date(item.fields.postedOn).toDateString();
 	  }
-        if(hasField("postHtml")&&!hasField("summaryLength")){
-			return item.fields.postHtml; 	
-		}
+        
 		
 		if(hasField("postHtml")&&hasField("summaryLength")){
 			
 			var $ = cheerio.load(item.fields.postHtml);
 			item.fields.postHtml = $('<div/>').append($("*").slice(0,preferences.summaryLength)).html();
-			return item.fields.postHtml;
+			 item.fields.postHtml;
 		}
+		
+		return item;
     });
     
     return data;
@@ -25,4 +25,7 @@ exports.prepareResponse = function (data){
 function hasField(field){
 	
 	return preferences.pageFields.indexOf(field)>-1;
+};
+
+function fullPost(){
 }
