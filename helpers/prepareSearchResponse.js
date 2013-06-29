4689df265276866fd986d23780c214d37b2c5fc5
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-29 20:12:30 +0530

    preparing response for search

---
+++ helpers/prepareSearchResponse.js
@@ -0,0 +1,59 @@
+var cheerio = require('cheerio');
+var preferences = require('../preferences').preferences;
+
+exports.prepareResponse = function (data){
+	
+	if(hasField("postHtml")&&preferences.searchResultsSummaryLength>0&&hasField("postedOn")){
+		preparePostSummaryAndDate(data);
+	}
+	
+	if(hasField("postHtml")&&preferences.summaryLength>0&& !hasField("postedOn")){
+		preparePostSummary(data);
+	}
+	
+	if(hasField("postedOn")){
+		
+		preparePostDate(data);
+		
+	   }
+    
+    return data;
+};
+
+function hasField(field){
+	
+	return preferences.searchResultsFileds.indexOf(field)>-1;
+};
+
+
+function preparePostSummaryAndDate(data){
+	data.forEach(function(item,index,arr){        
+      
+		var summary =  getPostSummary(item);
+		 summary.fields.postedOn = new Date(item.fields.postedOn).toDateString();
+		return summary;
+    });
+    return data;
+}
+
+function preparePostSummary(data){
+	
+	data.forEach(function(item,index,arr){      
+		  	return getPostSummary(item);
+    });
+    return data;
+}
+
+function preparePostDate(data){
+	data.forEach(function(item,index,arr){      
+		   item.fields.postedOn = new Date(item.fields.postedOn).toDateString();			
+			return item;
+    });
+    return data;
+}
+
+function getPostSummary(item){
+	var $ = cheerio.load(item.fields.postHtml);
+			item.fields.postHtml = $('<div/>').append($("*").slice(0,preferences.summaryLength)).html();
+			return item;
+};
