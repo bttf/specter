07c08b9805db818cf329ada7af4bc62beb69ff0c
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-29 20:16:06 +0530

    fleshed out

--- routes/search.js
+++ routes/search.js
@@ -16,6 +16,45 @@ exports.searchByTitle = function(req,res){
     
 };
 
+
+
+exports.deepSearch = function(req,res){
+    
+    var url = constants.queries.search();
+	var pageNo = parseInt(req.params.page);
+    var headers = helpers.setHeaders(url,getSearchPostsQueryData(pageNo,constants.queries.paginationSize));
+    
+    request(headers,function(error,response,body){
+		
+        var resultCount = constants.queries.paginationSize - 1;
+        var results = body.hits;
+        var dataToRender = buildResponse(results.hits.slice(0,resultCount),pageNo,body.hits.hits.length);
+		return res.render(constants.views.home,dataToRender);
+    });
+    
+    
+};
+
+function getSearchPostsQueryData(pageNo,paginationSize){    
+	
+    var queryData = {
+      "sort" :{ "postedOn" : {"order" : "desc"}},
+	   "fields" : preferences.pageFields	
+    };
+	
+    return helpers.pagination.buildPaginationQuery(pageNo,paginationSize,queryData);
+}
+
+function buildResponse(data,pageNo,total){
+       var items = {};
+    
+    items.hits = helpers.prepareResponse(data); 
+    items.hasPrevious = helpers.pagination.hasPrevButton(pageNo);
+    items.hasNext = helpers.pagination.hasNextButton(pageNo,total,constants.queries.paginationSize);
+	items.isFirstPage = helpers.pagination.isFirstPage(items.hasPrevious);
+    return items;
+
+
 function buildSearchQuery(searchTerm,isDeepSearch){
     
 var query = {  
