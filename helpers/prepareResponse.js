db629ba7a12557ce284d1aa99e6ce7abc0791c6b
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-06-29 13:08:16 +0530

    broken the method into testable chunks

--- helpers/prepareResponse.js
+++ helpers/prepareResponse.js
@@ -2,22 +2,20 @@ var cheerio = require('cheerio');
 var preferences = require('../preferences').preferences;
 
 exports.prepareResponse = function (data){
-	//check preferences here.
-    data.forEach(function(item,index,arr){        
-      if(hasField("postedOn")){
-		   item.fields.postedOn = new Date(item.fields.postedOn).toDateString();
-	  }
-        
-		
-		if(hasField("postHtml")&&preferences.summaryLength>0){
-			
-			var $ = cheerio.load(item.fields.postHtml);
-			item.fields.postHtml = $('<div/>').append($("*").slice(0,preferences.summaryLength)).html();
-			console.log(item.fields.postHtml);
+	
+	if(hasField("postHtml")&&preferences.summaryLength>0&&hasField("postedOn")){
+		preparePostSummaryAndDate(data);
 	}
+	
+	if(hasField("postHtml")&&preferences.summaryLength>0&& !hasField("postedOn")){
+		preparePostSummary(data);
+	}
+	
+	if(hasField("postedOn")){
 		
-		return item;
-    });
+		preparePostDate(data);
+		
+	   }
     
     return data;
 };
@@ -27,5 +25,35 @@ function hasField(field){
 	return preferences.pageFields.indexOf(field)>-1;
 };
 
-function fullPost(){
+
+function preparePostSummaryAndDate(data){
+	data.forEach(function(item,index,arr){        
+      
+		var summary =  getPostSummary(item);
+		 summary.fields.postedOn = new Date(item.fields.postedOn).toDateString();
+		return summary;
+    });
+    return data;
 }
+
+function preparePostSummary(data){
+	
+	data.forEach(function(item,index,arr){      
+		  	return getPostSummary(item);
+    });
+    return data;
+}
+
+function preparePostDate(data){
+	data.forEach(function(item,index,arr){      
+		   item.fields.postedOn = new Date(item.fields.postedOn).toDateString();			
+			return item;
+    });
+    return data;
+}
+
+function getPostSummary(item){
+	var $ = cheerio.load(item.fields.postHtml);
+			item.fields.postHtml = $('<div/>').append($("*").slice(0,preferences.summaryLength)).html();
+			return item;
+};
