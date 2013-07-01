1c9ea014975ab7be937c68adfaa9ad1ce7b0fdc2
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-01 18:51:50 +0530

    modifed the behaviour to pass preferences as parameter

--- helpers/prepareResponse.js
+++ helpers/prepareResponse.js
@@ -1,10 +1,10 @@
-var preferences = require('../preferences').preferences;
 var commonHelpers = require('./prepareResponseHelpers');
 
-exports.prepareResponse = function (data,fields){
+exports.prepareResponse = function (data,preferences){
 	
 	var summaryLength = preferences.summaryLength;
-	var hasField = commonHelpers.hasField;	
+	var hasField = commonHelpers.hasField;
+	var fields = preferences.pageFields; 	
 	var hasFieldPostHtml = hasField("postHtml",fields);
 	var hasFiledPostedOn = hasField("postedOn",fields);
 	
