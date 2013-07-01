ef5327883afb5469ddd4b07fc91daf424e536711
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-01 18:36:12 +0530

    parametrized fields
    so one method to prepare response

--- helpers/prepareResponse.js
+++ helpers/prepareResponse.js
@@ -1,39 +1,12 @@
 var preferences = require('../preferences').preferences;
-var commonHelpers = require('./commonHelpers');
+var commonHelpers = require('./prepareResponseHelpers');
 
-exports.prepareResponse = function (data){
+exports.prepareResponse = function (data,fields){
 	
 	var summaryLength = preferences.summaryLength;
-	var hasField = commonHelpers.hasField;
-	var pageFileds = preferences.pageFields;
-	var hasFieldPostHtml = hasField("postHtml",pageFileds);
-	var hasFiledPostedOn = hasField("postedOn",pageFileds);
-	
-	if(hasFieldPostHtml && summaryLength>0 && hasFiledPostedOn){
-		commonHelpers.preparePostSummaryAndDate(data,summaryLength);
-	}
-	
-	if(hasFieldPostHtml&& summaryLength>0 && ! hasFiledPostedOn){
-		commonHelpers.preparePostSummary(data,summaryLength);
-	}
-	
-	if(hasFiledPostedOn){
-		
-		commonHelpers.preparePostDate(data);
-		
-	   }
-    
-    return data;
-};
-
-
-exports.prepareSearchResponse = function (data){
-	
-	var summaryLength = preferences.summaryLength;
-	var hasField = commonHelpers.hasField;
-	var searchFileds = preferences.searchResultsFileds;
-	var hasFieldPostHtml = hasField("postHtml",searchFileds);
-	var hasFiledPostedOn = hasField("postedOn",searchFileds);
+	var hasField = commonHelpers.hasField;	
+	var hasFieldPostHtml = hasField("postHtml",fields);
+	var hasFiledPostedOn = hasField("postedOn",fields);
 	
 	if(hasFieldPostHtml && summaryLength>0 && hasFiledPostedOn){
 		commonHelpers.preparePostSummaryAndDate(data,summaryLength);
