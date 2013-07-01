76ec720db3df59a26731618c53043bfcecde5fcf
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-01 17:48:39 +0530

    getting ready for drying up prepare response methods

---
+++ helpers/commonHelpers.js
@@ -0,0 +1,39 @@
+exports.hasField = function (field,fields){
+	
+	return fields.indexOf(field)>-1;
+};
+
+
+exports.preparePostSummaryAndDate = function(data){
+	data.forEach(function(item,index,arr){        
+      
+		var summary =  getPostSummary(item);
+		 summary.fields.postedOn = new Date(item.fields.postedOn).toDateString();
+		return summary;
+    });
+    return data;
+};
+
+
+
+exports.preparePostSummary = function(data){
+	
+	data.forEach(function(item,index,arr){      
+		  	return getPostSummary(item);
+    });
+    return data;
+};
+
+exports.preparePostDate = function (data){
+	data.forEach(function(item,index,arr){      
+		   item.fields.postedOn = new Date(item.fields.postedOn).toDateString();			
+			return item;
+    });
+    return data;
+};
+
+exports.getPostSummary = function (item,preferredSummaryLength){
+	var $ = cheerio.load();
+	item.fields.postHtml = $("<div></div>").html($(item.fields.postHtml.replace(/(\r\n|\n|\r)/gm,"")).slice(0,preferredSummaryLength)).html();
+	return item;
+};
