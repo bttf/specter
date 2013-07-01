206a5668c28451966be91954eb127507acbfcac8
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-01 18:49:49 +0530

    preferences schema altered
    search index and normal index preferences are now objects

--- preferences.js
+++ preferences.js
@@ -1,13 +1,22 @@
 exports.preferences = {
 	// reference :	https://github.com/brislink/specter/wiki/preferences-documentation 
+	
 	"paginationSize" :11, 
-	 "databaseUrl"   :"http://localhost:9200/",
+	
+	"databaseUrl"   :"http://localhost:9200/",	
+	
+	"index" : {
 	"searchResults" :5,
 	"pageFields":["postedBy","postedOn","title","wordCount","postHtml"],
-	"summaryLength" : 3,
-	"searchResultsFileds" : ["postedBy","postedOn","title","wordCount","postHtml"],
-	"searchResultsSummaryLength" : 2,
-	"searchPageSize" : 11
+	"summaryLength" : 3
+	},
+	
+	"searchIndex" : {
+		
+	"pageFields" : ["postedBy","postedOn","title","wordCount","postHtml"],
+	"summaryLength" : 2,
+	"paginationSize" : 11
+}
 	
 	
 };
