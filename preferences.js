bf294d0aa13fb0fcb5485010d3540b8b1bcd37ce
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-01 22:43:46 +0530

    moved search results out of the object

--- preferences.js
+++ preferences.js
@@ -1,10 +1,10 @@
 exports.preferences = {
 	// reference :	https://github.com/brislink/specter/wiki/preferences-documentation 	 
 	
-	"databaseUrl"   :"http://localhost:9200/",	
-	
+	"databaseUrl"   :"http://localhost:9200/",
+	"searchResults" :5	
 	"index" : {
-	"searchResults" :5,
+	
 	"pageFields":["postedBy","postedOn","title","wordCount","postHtml"],
 	"summaryLength" : 3,
 	"paginationSize" :11
