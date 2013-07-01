f0c74ebe559647a12f967473705a5a379167df78
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-01 17:51:00 +0530

    exported the common helpers
    need to call them in the parent files now

--- helpers/index.js
+++ helpers/index.js
@@ -2,6 +2,7 @@ exports.getContributor = require('./getUserRole');
 exports.setHeaders = require('./setHeaders').setHeaders;
 exports.authorization = require('./authorization');
 exports.pagination = require('./pagination').pagination;
+exports.commonHelpers = require('./commonHelpers');
 exports.prepareResponse = require('./prepareResponse').prepareResponse;
 exports.prepareSearchResponse = require('./prepareSearchResponse').prepareSearchResponse;
 exports.stripHtml  = function (html){
