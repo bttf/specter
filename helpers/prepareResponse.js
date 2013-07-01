6827897afdb7e5aa3551651150a17e8fc66bf06b
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-01 17:52:46 +0530

    exposed in the prepare response itself
    no need to make it public

--- helpers/prepareResponse.js
+++ helpers/prepareResponse.js
@@ -1,5 +1,6 @@
 var cheerio = require('cheerio');
 var preferences = require('../preferences').preferences;
+var commonHelpers = require('./commonHelpers');
 
 exports.prepareResponse = function (data){
 	
@@ -20,40 +21,4 @@ exports.prepareResponse = function (data){
     return data;
 };
 
-function hasField(field){
-	
-	return preferences.pageFields.indexOf(field)>-1;
-};
 
-
-function preparePostSummaryAndDate(data){
-	data.forEach(function(item,index,arr){        
-      
-		var summary =  getPostSummary(item);
-		 summary.fields.postedOn = new Date(item.fields.postedOn).toDateString();
-		return summary;
-    });
-    return data;
-}
-
-function preparePostSummary(data){
-	
-	data.forEach(function(item,index,arr){      
-		  	return getPostSummary(item);
-    });
-    return data;
-}
-
-function preparePostDate(data){
-	data.forEach(function(item,index,arr){      
-		   item.fields.postedOn = new Date(item.fields.postedOn).toDateString();			
-			return item;
-    });
-    return data;
-}
-
-function getPostSummary(item){
-	var $ = cheerio.load();
-	item.fields.postHtml = $("<div></div>").html($(item.fields.postHtml.replace(/(\r\n|\n|\r)/gm,"")).slice(0,preferences.summaryLength)).html();
-	return item;
-};
