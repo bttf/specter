1afea384b4f59d76e6cf4f4881a51bb1b0b3dddb
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-03 20:01:14 +0530

    built query method for feeds

--- routes/feed.js
+++ routes/feed.js
@@ -1,1 +1,21 @@
 var feed = require('feed');
+var request = require('request');
+var preferences = require('preferences').preferences
+
+exports.getFeeds = function(req,res){
+	
+	var type = req.params.type;
+};
+
+
+function getRecentFeedsQuery(){
+	
+	var queryData = {
+      "sort" :{ "postedOn" : {"order" : "desc"}},
+	   "fields" :['postedOn','postTitle','postedBy'],
+		"from" : 0,
+		"size" : preferences.feeds.paginationSize
+    };
+	
+	return queryData;
+}
