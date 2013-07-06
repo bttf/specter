2f60c143d387849c4b3699de1f8373718ac41eac
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-06 17:08:25 +0530

    Added feed to the route configuration

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,4 @@ exports.search = require('./search');
 exports.edit = require('./edit');
 exports.update = require('./update');
 exports.delete = require('./deletePost');
+exports.feeds = require('./feeds');
