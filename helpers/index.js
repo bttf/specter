ddc6d58debd98dc51e9bb8c2525e803a6c2dd2b1
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-06 23:08:19 +0530

    build response method abstracted completely
    it is so dry ed up I ca see the cracks

--- helpers/index.js
+++ helpers/index.js
@@ -5,7 +5,7 @@ exports.getContributor = require('./getUserRole');
 exports.setHeaders = require('./setHeaders').setHeaders;
 exports.authorization = require('./authorization');
 exports.pagination = require('./pagination').pagination;
-exports.prepareResponse = require('./prepareResponse').prepareResponse;
+exports.buildResponse = require('./prepareResponse').buildResponse;
 exports.getPostSummary = function(item,preferredSummaryLength){
 	
 	item = $("<div></div>").html($(item.replace(/(\r\n|\n|\r)/gm,"")).slice(0,preferredSummaryLength)).html();
