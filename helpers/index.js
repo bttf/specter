1545212a53fc83b4ca0bc494d0c193be6e2ce436
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-06 17:56:31 +0530

    exported the getPostSummary

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,13 @@
+var cheerio = require('cheerio');
+var $ = cheerio.load();
+
 exports.getContributor = require('./getUserRole');
 exports.setHeaders = require('./setHeaders').setHeaders;
 exports.authorization = require('./authorization');
 exports.pagination = require('./pagination').pagination;
 exports.prepareResponse = require('./prepareResponse').prepareResponse;
+exports.getPostSummary = function(item,preferredSummaryLength){
+	
+	item.fields.postHtml = $("<div></div>").html($(item.fields.postHtml.replace(/(\r\n|\n|\r)/gm,"")).slice(0,preferredSummaryLength)).html();
+	return item;
+};
