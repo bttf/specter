c5d2f17c22feda7c6415a8c090641046a5bf4885
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-07 15:25:10 +0530

    Added more files to buildify

--- public/javascripts/Abstract/build.js
+++ public/javascripts/Abstract/build.js
@@ -1,7 +1,7 @@
 var buildify = require('buildify');
 
 buildify()
-  .concat(['jquery.autosize.js','markdown.js','StaticVariables.js', 'models.js','helpers.js','drafts.js']).wrap('base.js').save('./index.js')
+  .concat(['jquery.autosize.js','marked.js','StaticVariables.js', 'models.js','helpers.js','drafts.js','remarked.js']).wrap('base.js').save('./index.js')
 .uglify().save('./index.min.js');
   
 
