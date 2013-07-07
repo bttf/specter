16893b7909823e92635e92745cb77bd7714b03ad
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-07 16:46:23 +0530

    It is working but few issues need to be ironed out

--- routes/edit.js
+++ routes/edit.js
@@ -8,15 +8,16 @@ exports.editPost = function(req,res){
     var url = constants.queries.postType() + id;
      request(url,function(error,response,body){
         var parsed = JSON.parse(body);
+		 
         if(!parsed._source) return res.send(404);        
        return res.render(constants.views.createPost,buildData(parsed));
     });
 };
 
-function buildData(markdownText,parsed){
+function buildData(parsed){
 
  var item = {
-    post :parsed_source.postHtml,
+    post :parsed._source.postHtml,
     title : parsed._source.title,
      postedBy : parsed._source.postedBy,
      id : parsed._id,
