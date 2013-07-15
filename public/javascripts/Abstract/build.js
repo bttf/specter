7a49879f68c551587c18226111f8a9b6aff856fb
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-15 19:47:48 +0530

    Added tag input plugin
    seems good

--- public/javascripts/Abstract/build.js
+++ public/javascripts/Abstract/build.js
@@ -1,7 +1,7 @@
 var buildify = require('buildify');
 
 buildify()
-  .concat(['jquery.autosize.js','marked.js','StaticVariables.js', 'models.js','helpers.js','drafts.js','remarked.js']).wrap('base.js').save('./index.js')
+  .concat(['jquery.autosize.js','jquery.tagsinput.js','marked.js','StaticVariables.js', 'models.js','helpers.js','drafts.js','remarked.js']).wrap('base.js').save('./index.js')
 .uglify().save('./index.min.js');
   
 
