968f4a3c189b7b7532ef06eecc128a5839629dc9
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-16 19:07:43 +0530

    build complete

--- public/javascripts/Abstract/index.js
+++ public/javascripts/Abstract/index.js
@@ -2603,16 +2603,7 @@ reMarked = function(opts) {
   * License MIT (c) Dustin Diaz & Jacob Thornton 2012
   */
 !function(a,b){typeof define=="function"?define(b):typeof module!="undefined"?module.exports=b():this[a]=b()}("klass",function(){function f(a){return j.call(g(a)?a:function(){},a,1)}function g(a){return typeof a===c}function h(a,b,c){return function(){var d=this.supr;this.supr=c[e][a];var f=b.apply(this,arguments);return this.supr=d,f}}function i(a,b,c){for(var f in b)b.hasOwnProperty(f)&&(a[f]=g(b[f])&&g(c[e][f])&&d.test(b[f])?h(f,b[f],c):b[f])}function j(a,b){function c(){}function l(){this.init?this.init.apply(this,arguments):(b||h&&d.apply(this,arguments),j.apply(this,arguments))}c[e]=this[e];var d=this,f=new c,h=g(a),j=h?a:this,k=h?{}:a;return l.methods=function(a){return i(f,a,d),l[e]=f,this},l.methods.call(l,k).prototype.constructor=l,l.extend=arguments.callee,l[e].implement=l.statics=function(a,b){return a=typeof a=="string"?function(){var c={};return c[a]=b,c}():a,i(this,a,d),this},l}var a=this,b=a.klass,c="function",d=/xyz/.test(function(){xyz})?/\bsupr\b/:/.*/,e="prototype";return f.noConflict=function(){return a.klass=b,this},a.klass=f,f});
-function createTaggedDraft (){
-			var key = titleContainer.val();
-        	var draft = {};
-            draft["time"] = new Date();
-        	draft["text"] = getMarkdownText();
-        	draft["wordCount"] =getWordCountFromLabel(wordCountLabel.text());
-			draft["tags"] = tags.val();
-        	localStorage.setItem(key, JSON.stringify(draft));
-		}
-				
+			
 				$("#tags").tagsInput({'defaultText':' Click to add tags','height':'0px','onAddTag':function(param){
 					saveCurrentDraft(titleContainer.text());
 				},'onRemoveTag':function(param){
