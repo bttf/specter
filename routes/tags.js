24abb504c55d3237678412bb652da9ebcf9211d4
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-07-20 19:43:09 +0530

    Index supplied with the commons object

--- routes/tags.js
+++ routes/tags.js
@@ -4,12 +4,38 @@ var preferences = require('../preferences').preferences;
 
 
 exports.getTaggedPosts = function(req,res){
+	
+	var url = constants.queries.search();
+	var pageNo = parseInt(req.params.page);
+	var paginationSize  = preferences.tagIndex.paginationSize;
+	var tag = req.params.tag;
+    var headers = helpers.setHeaders(url,buildTagQuery(tag,pageNo,paginationSize));
+    
+    request(headers,function(error,response,body){		
+				
+		var total = body.hits.hits.length;
+        var resultCount = paginationSize - 1;
+        var results = body.hits;
+		
+		var common = {
+			
+			data : results.hits.slice(0,resultCount),
+			pageNo : pageNo,
+			total: total,
+			preferences : preferences,
+			index: preferences.tagIndex
+		}
+		
+        var dataToRender = helpers.buildResponse(common);
+		return res.render(constants.views.home,dataToRender);
+    });
+    
 }
 
-function buildTagQuery(tag){
+function buildTagQuery(tag,pageNo,paginationSize){
 	
-	var query = {
-		"fields" : preferences.tagIndex.fields,
+	var queryData = {
+		"fields" : preferences.tagIndex.pageFields,
 		"size" : preferences.tagIndex.paginationSize,
 		"query" : {
 			"term" :{
@@ -17,5 +43,7 @@ function buildTagQuery(tag){
 			}
 		}
 	}
+	
+	return helpers.pagination.buildPaginationQuery(pageNo,paginationSize,queryData);
 	
 }
