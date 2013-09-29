75ef39cdb82583cbc5039b9833f196d650a06b94
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-09-29 15:25:24 +0530

    create and search routes at the top to intercept requests

--- routeConfiguration.js
+++ routeConfiguration.js
@@ -1,7 +1,8 @@
 var routes = require('./routes');
 
 exports.defineRoutes= function(app){
-    
+app.get('/create',routes.create.newPost);
+app.get('/search',routes.search.deepSearch);    
 app.get('/api/posts/recent',routes.recentPosts.getRecentPostsAPI);
 app.get('/blog/archives',routes.archives.getArchives);
 app.get('/feed/:type',routes.feeds.getFeeds);	
@@ -11,8 +12,6 @@ app.get('/tag/:tag/:page',routes.tags.getTaggedPosts);
 app.get('/:id',routes.postDetail.postDetail);
 app.get('/:id/edit',routes.edit.editPost);
 app.get('/:id/delete',routes.delete.getPostToDelete);
-app.get('/create',routes.create.newPost);
-app.get('/search',routes.search.deepSearch);
 app.get('/',routes.recentPosts.getRecentPosts);
 app.post('/searchByTitle',routes.search.searchByTitle);    
 app.post('/addpost',routes.create.addPost);
