66ed2d48f60fca0fdb7fe983504a293105c39a76
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-09-29 15:31:40 +0530

    Route configration commented and routes seperated by their fuinction

--- routeConfiguration.js
+++ routeConfiguration.js
@@ -1,18 +1,26 @@
 var routes = require('./routes');
 
 exports.defineRoutes= function(app){
+
+// Reserved routes
 app.get('/create',routes.create.newPost);
-app.get('/search',routes.search.deepSearch);    
+app.get('/search',routes.search.deepSearch);
+
+// Routes for API
 app.get('/api/posts/recent',routes.recentPosts.getRecentPostsAPI);
+
+// GET Routes 
 app.get('/blog/archives',routes.archives.getArchives);
 app.get('/feed/:type',routes.feeds.getFeeds);	
 app.get('/page/:page',routes.recentPosts.getRecentPosts);
 app.get('/tag/:tag',routes.tags.getTaggedPosts);
 app.get('/tag/:tag/:page',routes.tags.getTaggedPosts);	
-app.get('/:id',routes.postDetail.postDetail);
 app.get('/:id/edit',routes.edit.editPost);
 app.get('/:id/delete',routes.delete.getPostToDelete);
+app.get('/:id',routes.postDetail.postDetail);
 app.get('/',routes.recentPosts.getRecentPosts);
+
+// POST ROUTES
 app.post('/searchByTitle',routes.search.searchByTitle);    
 app.post('/addpost',routes.create.addPost);
 app.post('/updatePost',routes.update.updatePost);    
