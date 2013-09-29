da95b8217de413f9e4bd5318a209545f7b87202d
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-09-29 19:52:46 +0530

    moved archives to the core

--- core/index.js
+++ core/index.js
@@ -1,2 +1,3 @@
 exports.getRecentPosts = require('./recentPosts').getRecentPosts;
 exports.getTaggedPosts = require('./tags').getTaggedPosts;
+exports.getArchives = require('./archives').getArchives;
--- routes/archives.js
+++ routes/archives.js
@@ -1,50 +1,9 @@
-var request = require('request');
-var helpers = require('../helpers');
-var constants = require('../constants');
+var core = require('../core');
 
 exports.getArchives = function(req,res){
-request(buildArchivesQuery(),function(error,response,body){
 
-	if(error||!body) res.send(500);
-	
-	var data = buildResponse(body.hits.hits); 
-	return res.render(constants.views.archives,data);
-});
-
-}
-
-function buildArchivesQuery(){
-
-	var query = {
-		"fields": ["postedBy","postedOn","title","wordCount","tags"],
-		"size" : 1000000,
-		"sort" :{ "postedOn" : {"order" : "asc"}},
-		"query" : {
-
-		"match_all" :{}
-	}
-		
 };
 
-	var url = constants.queries.search();
-	var headers = helpers.setHeaders(url,query);
-	return headers;
-}
+exports.getArchivesAPI = function(req,res){
 
-function buildResponse(data){
-
-	var uniqueTags = [];
-
-	data.forEach(function(item){
-
-		item.fields.postedOn = new Date(item.fields.postedOn).toDateString(); 
-		
-		var intersection = item.fields.tags.filter(function(tag){return uniqueTags.indexOf(tag)<0});
-	
-		uniqueTags.push.apply(uniqueTags,intersection);
-		return item;
-	});
-
-	
-	return {archives:data,archiveTags: uniqueTags};
-}
+};
