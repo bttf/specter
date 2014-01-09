38820d1ac291b27e335087df3a4babe3febabf50
bttf <adnanchowdhury88@gmail.com>
2014-01-09 02:14:18 +0000

    Indents

--- constants/databaseQueries.js
+++ constants/databaseQueries.js
@@ -1,24 +1,24 @@
 var preferences = require('../preferences').preferences;
 
 exports.queries = {
-    
-     databaseUrl : preferences.databaseUrl,   
-	
-     searchSize : preferences.searchResults,
-    
-     blogIndex : function(){
-         
-         return this.databaseUrl+'blog/';
-     },
-    
-    postType : function(){
-        
-        return this.blogIndex()+'post/';
-    },
-    
-    search : function(){
-    
+
+  databaseUrl : preferences.databaseUrl,   
+
+  searchSize : preferences.searchResults,
+
+  blogIndex : function(){
+
+    return this.databaseUrl+'blog/';
+  },
+
+  postType : function(){
+
+    return this.blogIndex()+'post/';
+  },
+
+  search : function(){
+
     return this.postType()+'_search';
-    
-    }
+
+  }
 };
